1023ccd10f753eb8aa3409b4e934a76e22b8b7d3
nuel-ikwuoma <emmanuelikwuoma7@gmail.com>
2023-08-27 22:59:00 +0100

    siwe verification backend

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ require("express-async-errors");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
-
+import { generateNonce, SiweMessage } from 'siwe';
 
 // const multer = require("multer");
 // const upload = multer({ dest: "uploads/" });
@@ -34,6 +34,15 @@ app.use(express.json());
 // MORGAN SETUP
 app.use(morgan("dev"));
 
+// SESSION
+app.use(Session({
+    name: 'siwe-aikicloud',
+    secret: process.env.SIWE_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false, sameSite: true }
+}));
+
 // ######################################################################################################
 // ######################################################################################################
 // Routes
@@ -48,8 +57,42 @@ app.get(
     }
 );
 
+// Get a random nonce
+app.get('/nonce', async function (req, res) {
+    req.session.nonce = generateNonce();
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200).send(req.session.nonce);
+});
+
+
 // VM ROUTES
 const vmRoutes = require("./routes/vm");
+app.use("/api/vm", async function (req, res, next) {
+    // the token id is passed in the body
+    const tokenId = req.body.tokenId;
+    try {
+        if (!req.body.message) {
+            res.status(422).json({ message: 'Message Not found.' });
+            return;
+        }
+
+        let SIWEObject = new SiweMessage(req.body.message);
+        const { data: message } = await SIWEObject.verify({ signature: req.body.signature, nonce: req.session.nonce });
+
+        console.log(message);
+        
+        req.session.siwe = message;
+        req.session.cookie.expires = new Date(message.expirationTime);
+        req.session.save(() => res.status(200).send(true));
+        next();
+    } catch (e) {
+        req.session.siwe = null;
+        req.session.nonce = null;
+        console.error(e);
+        req.session.save();
+        res.status(440).json({ message: e.message })
+    }
+});
 app.use("/api/vm", vmRoutes);
 
 // ######################################################################################################
