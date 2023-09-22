d6ff16d824add04a86468c9f0574aa1c64f20e88
Owolabi precious <96595416+Pr3c10us@users.noreply.github.com>
2023-09-22 13:11:15 +0100

    Puff (#18)
    * tesst on your end
    
    * done
    
    * cooked
    
    * added arbitrum logo
    
    * update
    
    * done
    
    * configured backend for vercel
    
    * Settled net switch and approval
    
    * made so allowance can also be same
    
    * Update next.config.js
    
    * Update next.config.js
    
    * Update next.config.js

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // redirect to gitcoin grantpage if user in /gitcoin
+    async redirects() {
+        return [
+            {
+                source: "/arb-grants",
+                destination:
+                    "https://explorer.gitcoin.co/#/round/42161/0x8b70206844630d8c0a2a545e92d3c8d46a3ceaad/0x8b70206844630d8c0a2a545e92d3c8d46a3ceaad-18",
+                permanent: false,
+                basePath: false,
+            },
+        ];
+    },
   images: {
     remotePatterns: [
       {
