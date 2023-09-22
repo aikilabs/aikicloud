a6f26e4513b422d61412d7029a44083a4504d3cb
Owolabi precious <96595416+Pr3c10us@users.noreply.github.com>
2023-09-22 11:55:04 +0100

    Update next.config.js

--- ui/next.config.js
+++ ui/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     async redirects() {
         return [
             {
-                source: "/gitcoin",
+                source: "/arb-grants",
                 destination:
                     "https://explorer.gitcoin.co/#/round/42161/0x8b70206844630d8c0a2a545e92d3c8d46a3ceaad/0x8b70206844630d8c0a2a545e92d3c8d46a3ceaad-18",
                 permanent: false,
